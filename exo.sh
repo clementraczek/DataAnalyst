032b542bb95a89e734308012a0b6a09aff9411ca
= <clement.raczek@labom2iformation.fr>
2025-10-30 14:31:11 +0100

    first commit

---
+++ exo.sh
@@ -0,0 +1,44 @@
+#exo 2
+$ git echo "ddd" > fichier3.txt
+$ git add fichier3.txt
+$ git commit -m "ajout de fichier txt"
+git log
+$ git commit -m "blabla"
+git revert HEAD
+git log
+git branch branche1
+git commit 
+git log branch1
+git branch
+git git tag v1.0
+$ git reset --soft HEAD
+git tag
+git branch -d branche1
+git branch
+
+
+#exo3
+mkdir projet2
+cd projet2
+git echo "kjhjjk" > fichier.txt
+git add fichier.txt
+$ git commit -m "modif2"
+$ git checkout HEAD~1
+$ git checkout -b B1
+$ echo "ligne modifié3" > fichier .txt
+$ git add fichier.txt
+$ git commit -m "modif3"
+$ git checkout main
+$ git merge B1
+
+#exo4
+
+
+
+
+
+
+
+
+
+
